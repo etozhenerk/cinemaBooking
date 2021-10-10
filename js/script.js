62f65b439f28198a45e08ab268f3a351bccf328b
Mikhail <etozhenerk@yandex.ru>
2021-10-10 20:09:12 +0300

    add calc

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 "use strict";
 
 const schemeSvg = document.querySelector(".scheme__svg");
+const priceTotalTag = document.querySelector('.price__total');
+
+
+let cost = 800;
+let totalPrice = 0;
+
 schemeSvg.addEventListener("click", (e) => {
+
     const target = e.target;
     if (!target.classList.contains("booked")) {
         target.classList.toggle("active");
+        let acrive = schemeSvg.querySelectorAll('.active').length;
+        totalPrice = acrive * cost;
+        priceTotalTag.textContent = totalPrice;
     }
 });
